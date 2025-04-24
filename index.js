2b505c879ce63fbac6a38af11330d34e354134c1
Brenda <brendanjeri2001@gmail.com>
2025-04-24 14:52:47 -0700

    changes

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 console.log("hello world");
 let pi;
-pi
+pi = 3.142;
+const r = 7;
+let c = pi * r * r
+console.log(c);
+const CurrentUser = 'Brenda Njeri';
+const WelcomeMessage = 'Welcome to my website';
+console.log("CurrentUser".length);
+console.log("CurrentUser"[5]);
+console.log(WelcomeMessage, CurrentUser);
+/*another way to concatenate*/
+const CurrentUser1 = 'Brenda Nyagah';
+const WelcomeMessagenew = 'Welcome to my website ' + CurrentUser1;
+console.log(WelcomeMessagenew);
+console.log(10!=9);
+
+function testnum(a){
+    let results;
+    if(a>0){
+        results = "positive";
+    }
+    else{
+        results = "Not positive";
+    }
+    return results;
+}
+console.log(testnum(20));
